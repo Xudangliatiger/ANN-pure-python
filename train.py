dbdf07e894e8822e7722e965b66a1f2257727dfb
Xudangliatiger <xdlisatiger@outlook.com>
2018-11-18 16:15:54 +0800

    修改：     .idea/workspace.xml 	修改：     README.md 	新文件：   __pycache__/mnist_reader.cpython-37.pyc 	新文件：   __pycache__/model.cpython-37.pyc 	修改：     train.py

--- train.py
+++ train.py
@@ -25,20 +25,22 @@ def dataNormalize(data):
     data= (data-min)/(min+max)
     return  data
 
-x_train, y_train = mnist_reader.load_mnist('./data', kind='train')
-#x_test, y_test = mnist_reader.load_mnist('./data', kind='t10k')
-x_train =  dataNormalize(x_train)
-y_train = transform_one_hot(y_train)
 
-#定义模型
-ann = model.model(input_size=input_size,n_hidden=n_hidden,output_size=output_size,weight_path=weight_path,bias=None)
+if __name__ == '__main__':
+    x_train, y_train = mnist_reader.load_mnist('./data', kind='train')
+    #x_test, y_test = mnist_reader.load_mnist('./data', kind='t10k')
+    x_train =  dataNormalize(x_train)
+    y_train = transform_one_hot(y_train)
 
-for i in  range(epoch_size):
-    print('----------------------------------epoch',i+1,' is running')
-    # if i < 50:
-    #     lr = 0.000001
-    # else:
-    #     lr = 0.00001
-    ann.SGD(x_train,y_train,batch_size,lr)
-    if i % 2 == 0:
-        ann.saveModel()
+    #定义模型
+    ann = model.model(input_size=input_size,n_hidden=n_hidden,output_size=output_size,weight_path=weight_path,bias=None)
+
+    for i in  range(epoch_size):
+        print('----------------------------------epoch',i+1,' is running')
+        # if i < 50:
+        #     lr = 0.000001
+        # else:
+        #     lr = 0.00001
+        ann.SGD(x_train,y_train,batch_size,lr)
+        if i % 2 == 0:
+            ann.saveModel()
